235b9f4c9d6bf7ae5a36938f994de22ca7e1b30b
Cameron Ehrlich <cameronehrlich@gmail.com>
2026-08-04 22:06:10 -0700

    Add explicit training crawler policy (#13)

--- app/robots.ts
+++ app/robots.ts
@@ -3,10 +3,24 @@ import { siteConfig } from "@/data/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Applebot-Extended",
+        disallow: "/",
+      },
+    ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
   };
 }
